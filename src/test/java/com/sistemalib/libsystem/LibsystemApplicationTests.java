715477065c2c9f60b5ba8973ad258446946fcbe2
Antony Reis <antony.matheus@eyeflow.ai>
2024-11-07 18:36:50 -0300

    Initial commit

---
+++ src/test/java/com/sistemalib/libsystem/LibsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sistemalib.libsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
