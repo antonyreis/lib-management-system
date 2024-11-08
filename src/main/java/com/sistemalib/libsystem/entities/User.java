85a6d66c4ebf2be03a4dad6a7d53f62fb7a5e725
Antony Reis <antony.matheus@eyeflow.ai>
2024-11-08 12:19:43 -0300

    tests
    tests of api using an example of user

--- src/main/java/com/sistemalib/libsystem/entities/User.java
+++ src/main/java/com/sistemalib/libsystem/entities/User.java
@@ -1,10 +1,27 @@
 package com.sistemalib.libsystem.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
+import jakarta.persistence.Table;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+@Table(name = "tb_user")
 public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String email;
 	
+	@ManyToOne
+	@JoinColumn(name = "department_id")
 	private Department department;
 	
 	public User() {
